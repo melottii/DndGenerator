b2f934a740b5b877e689bed0dca1084a8b0033f0
Vinicius Da Hora Corte <20181BSI0105@cefetes.br>
2022-09-29 11:03:23 -0300

    init models

---
+++ models/CaracteristicaPersonagem.py
@@ -0,0 +1,8 @@
+class CaracteristicaPersonagem:
+    def __init__(self, nome, descricao, random, selecionavel, vida, nivel):
+        self.nome = nome
+        self.desricao = descricao
+        self.random = random
+        self.selecionavel = selecionavel
+        self.vida = vida
+        self.nivel = nivel
---
+++ models/Classe.py
@@ -0,0 +1,6 @@
+class Classe:
+    def __init__(self, nome, descricao, restricoes, habilidadesUnicas=[]):
+        self.nome = nome
+        self.descricao = descricao
+        self.restricoes = restricoes
+        self.habilidadesUnicas = habilidadesUnicas
